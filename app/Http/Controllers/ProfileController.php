8575171827ff58ca91f981b864aaac6231f97193
Boonnithi <boonnithi.c@gmail.com>
2018-07-14 00:45:47 +0700

    edit comment

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,9 +13,10 @@ class ProfileController extends Controller
     }
     public function change()
     {
-        $avatar = request()->file('avatar');//can add 'Request $request' at method's arguments, I use that becuase to try helper function for same results.
+        $avatar = request()->file('avatar');//can add 'Request $request' at method's arguments, I use that becuase to try helper function but it's put same results.
         $ext = $file->guessClientExtension();//can use $file->extension(); for same results. 
         $file->storeAs('avatar' . Auth::user()->id, 'avatar.' . $ext);
+
         return redirect('/home');
     }
 }
