d2b93efb9298673d456b8201e837296c167ea20c
Boonnithi <boonnithi.c@gmail.com>
2018-07-14 00:15:46 +0700

    add user detail

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,12 @@
    <div class="row">
 
       <div class="col-md-3">
-         <h3>{{ Auth::user()->name }} {{ Auth::user()->surname }}</h3>
+        <a href="/profile/change">Change Profile</a>
+         <h4><strong>{{ Auth::user()->name }} {{ Auth::user()->surname }} ( {{ Auth::user()->nickname }} )</strong></h4>
+         <p>{{Auth::user()->biography}}</p>
          <div class="list-group">
-            <p class="list-group-item-text">Education : </p>
-            <p class="list-group-item-text">Education : </p>
+            <p class="list-group-item-text">{{Auth::user()->education}}</p>
+            <p class="list-group-item-text">Website : <a href="{{Auth::user()->website}}">{{Auth::user()->website}}</a></p>
          </div>
       </div>
 
@@ -32,7 +34,7 @@
          @foreach ($posts->get() as $post)
          <div class="panel panel-default">
             <div class="panel-body">
-            <h4>{{$post->owner->name}} {{$post->owner->surname}}</h4> {{-- I'm so confused because why not use $post->owner()->name instead $post->owner->surname (why () missing) https://stackoverflow.com/questions/47010807/undefined-property-illuminate-database-eloquent-relations-belongstoname-lara OK, many people found this problems too and fix by same method--}}
+            <h4><strong>{{$post->owner->name}} {{$post->owner->surname}}</strong></h4> {{-- I'm so confused because why not use $post->owner()->name instead $post->owner->surname (why () missing) https://stackoverflow.com/questions/47010807/undefined-property-illuminate-database-eloquent-relations-belongstoname-lara OK, many people found this problems too and fix by same method--}}
             <small>{{$post->created_at}}</small>
             <p>{{$post->body}}</p>
             <a href="/like/{{$post->id}}"><strong>{{$post->likes}} Like(s)</strong></a>
