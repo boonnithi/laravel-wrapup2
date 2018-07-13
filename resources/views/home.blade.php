c8ff970d291bf25ee7f6afe80b57f1abbc56cd84
Boonnithi <boonnithi.c@gmail.com>
2018-07-14 00:38:46 +0700

    edit path of change profile form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
    <div class="row">
 
       <div class="col-md-3">
-        <a href="/profile/change">Change Profile</a>
+        <a href="/profile/changeform">Change Profile</a>
          <h4><strong>{{ Auth::user()->name }} {{ Auth::user()->surname }} ( {{ Auth::user()->nickname }} )</strong></h4>
          <p>{{Auth::user()->biography}}</p>
          <div class="list-group">
