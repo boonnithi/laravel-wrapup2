5d933a493c425235d1096f31740921ec5c768e94
Boonnithi <boonnithi.c@gmail.com>
2018-07-14 00:43:01 +0700

    add change method

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
 class ProfileController extends Controller
 {
     public function showChangeProfileForm(){
         return view('changeprofile');
+    }
+    public function change()
+    {
+        $avatar = request()->file('avatar');//can add 'Request $request' at method's arguments, I use that becuase to try helper function for same results.
+        $ext = $file->guessClientExtension();//can use $file->extension(); for same results. 
+        $file->storeAs('avatar' . Auth::user()->id, 'avatar.' . $ext);
+        return redirect('/home');
     }
 }
