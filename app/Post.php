e7c6e33084ddbd63718371547d770775e127e9a5
Boonnithi <boonnithi.c@gmail.com>
2018-07-13 23:22:26 +0700

    add missing method in post model

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = "Post";
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'ownerID');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'ownerPost');
+    }
 }
